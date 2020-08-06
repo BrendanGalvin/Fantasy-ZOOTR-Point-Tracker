1d1ea8f2ff140fdae67a32c041343acf0d5d4ae8
Brendan Galvin <codefun64@gmail.com>
2020-08-06 15:04:09 -0700

    Initial Version

---
+++ FantasyZOOTR/FantasyZOOTR/Points.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyZOOTR
+{
+    public static class Points
+    {
+        public static List<double> Hookshot = new List<double>();
+        public static List<double> Bow = new List<double>();
+        public static List<double> BombBag = new List<double>();
+        public static List<double> Strength = new List<double>();
+        public static List<double> Lights = new List<double>();
+        public static List<double> FireArrows = new List<double>();
+        public static List<double> IceArrows = new List<double>();
+        public static List<double> DinsFire = new List<double>();
+        public static List<double> FaroresWind = new List<double>();
+        public static List<double> NayrusLove = new List<double>();
+        public static List<double> MegatonHammer = new List<double>();
+        public static List<double> Irons = new List<double>();
+        public static List<double> Hovers = new List<double>();
+        public static List<double> MirrorShield = new List<double>();
+        public static List<double> Magic = new List<double>();
+        public static List<double> Slingshot = new List<double>();
+        public static List<double> Boomerang = new List<double>();
+        public static List<double> RutosLetter = new List<double>();
+        public static List<double> Bottle = new List<double>();
+        public static List<double> Scale = new List<double>();
+        public static List<double> Bombchu = new List<double>();
+        public static List<double> Wallet = new List<double>();
+        public static List<double> LensofTruth = new List<double>();
+        public static List<double> BGS = new List<double>();
+        public static List<double> KokiriSword = new List<double>();
+        public static List<double> GoronTunic = new List<double>();
+        public static List<double> ZoraTunic = new List<double>();
+        public static List<double> DekuStickUpgrade = new List<double>();
+        public static List<double> DekuNutUpgrade = new List<double>();
+        public static List<double> DekuStick = new List<double>();
+        public static List<double> DekuNut = new List<double>();
+        public static List<double> HylianShield = new List<double>();
+        public static List<double> DekuShield = new List<double>();
+        public static List<double> HugeRupee = new List<double>();
+        public static List<double> PurpleRupee = new List<double>();
+        public static List<double> RedRupee = new List<double>();
+        public static List<double> Bluepee = new List<double>();
+        public static List<double> GreenRupee = new List<double>();
+        public static List<double> StoneofAgony = new List<double>();
+        public static List<double> RecoveryHeart = new List<double>();
+        public static List<double> HeartContainer = new List<double>();
+        public static List<double> HeartPiece = new List<double>();
+        public static List<double> BGSTradeItem = new List<double>();
+        public static List<double> DoubleD = new List<double>();
+        public static List<double> IceTrap = new List<double>();
+        public static List<double> Winner = new List<double>();
+    }
+}
